aae226e5f3ef89b8fece856dc1b5ef1f47ba08cc
Michał Zieliński <michal@zielinscy.org.pl>
2015-09-14 21:00:10 +0200

    lzma: legacy LZMA-alone compressor

--- package/lzma/lzma.mk
+++ package/lzma/lzma.mk
@@ -4,10 +4,18 @@
 #
 ################################################################################
 
-LZMA_VERSION = 4.32.7
-LZMA_SOURCE = lzma-$(LZMA_VERSION).tar.xz
-LZMA_SITE = http://tukaani.org/lzma
+LZMA_VERSION = 4.65
+LZMA_SOURCE = lzma-$(LZMA_VERSION).tar.bz2
+LZMA_SITE = https://users.atomshare.net/~zlmch
 
-$(eval $(host-autotools-package))
+LZMA_ALONE_DIR = $(HOST_LZMA_BUILDDIR)/CPP/7zip/Compress/LZMA_Alone
 
-LZMA = $(HOST_DIR)/usr/bin/lzma
+define HOST_LZMA_BUILD_CMDS
+	$(MAKE) -C $(LZMA_ALONE_DIR) -f makefile.gcc
+endef
+
+define HOST_LZMA_INSTALL_CMDS
+	$(INSTALL) $(LZMA_ALONE_DIR)/lzma $(HOST_DIR)/usr/bin/lzma_alone
+endef
+
+$(eval $(host-generic-package))
